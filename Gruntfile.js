e7df9419d4d7780e97bb7fea3757d94d6bc91e57
Ross McKay <rmckay@webaware.com.au>
2016-06-02 16:09:22 +1000

    add grunt build config

---
+++ Gruntfile.js
@@ -0,0 +1,74 @@
+module.exports = function (grunt) {
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON("package.json"),
+
+		clean: [ "dist/**" ],
+
+		copy: {
+			main: {
+				files: [
+					{
+						src: ["./**", "!./node_modules/**", "!./Gruntfile.js", "!./package.json"],
+						dest: "dist/<%= pkg.name %>/"
+					}
+				]
+			}
+		},
+
+		compress: {
+			options: {
+				archive: "./dist/<%= pkg.name %>-<%= pkg.version %>.zip",
+				mode: "zip"
+			},
+			all: {
+				files: [{
+					expand: true,
+					cwd: "./dist/",
+					src: [ "<%= pkg.name %>/**" ]
+				}]
+			}
+		},
+
+		jshint: {
+			all: [
+				"Gruntfile.js",
+				"js/*.js",
+				"!js/*.min.js"
+			],
+			options: {
+				jshintrc: ".jshintrc",
+				force: true
+			}
+		},
+
+		uglify: {
+			build: {
+				options: {
+					preserveComments: /^!/
+				},
+				files: [{
+					expand: true,
+					cwd: "js",
+					dest: "js",
+					src: [
+						"*.js",
+						"!*.min.js"
+					],
+					ext: '.min.js'
+				}]
+			}
+		}
+
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks("grunt-contrib-compress");
+	grunt.loadNpmTasks("grunt-contrib-copy");
+	grunt.loadNpmTasks("grunt-contrib-jshint");
+	grunt.loadNpmTasks("grunt-contrib-uglify");
+
+	grunt.registerTask("release", ["clean","copy","compress"]);
+	grunt.registerTask("default", [ "jshint" ]);
+
+};
