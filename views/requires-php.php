f3f4da535c5a262c13d68a5826da9c86edbaea47
Ross McKay <rmckay@webaware.com.au>
2019-06-29 19:53:23 +1000

    update php.net links

--- views/requires-php.php
+++ views/requires-php.php
@@ -9,7 +9,7 @@ if (!defined('ABSPATH')) {
 		<?php echo gf_dpspxpay_external_link(
 				sprintf(esc_html__('Gravity Forms DPS PxPay requires PHP %1$s or higher; your website has PHP %2$s which is {{a}}old, obsolete, and unsupported{{/a}}.', 'gravity-forms-dps-pxpay'),
 					esc_html(GFDPSPXPAY_PLUGIN_MIN_PHP), esc_html(PHP_VERSION)),
-				'https://secure.php.net/supported-versions.php'
+				'https://www.php.net/supported-versions.php'
 			); ?>
 	</p>
 	<p><?php printf(esc_html__('Please upgrade your website hosting. At least PHP %s is recommended.', 'gravity-forms-dps-pxpay'), '7.2'); ?></p>
