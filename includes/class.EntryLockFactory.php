af0179a22c6a07b3d620643ee8b01e8ed6abcb40
Ross McKay <rmckay@webaware.com.au>
2024-11-13 13:47:46 +1100

    add persistent memory cache implementation of entry locks

--- includes/class.EntryLockFactory.php
+++ includes/class.EntryLockFactory.php
@@ -5,6 +5,8 @@ if (!defined('ABSPATH')) {
 	exit;
 }
 
+const OBJECT_CACHE_LOCK_GROUP = 'gfpxpay_locks';
+
 /**
  * interface for an entry lock object
  */
@@ -24,6 +26,12 @@ interface IEntryLock {
 class EntryLockFactory {
 
 	public static function create(string $id) : IEntryLock {
+		// try to use a persistent object cache first
+		if (wp_using_ext_object_cache()) {
+			return new EntryLockObjectCache($id);
+		}
+
+		// fallback to the database
 		return new EntryLockMySQL($id);
 	}
 
@@ -68,6 +76,41 @@ class EntryLockMySQL implements IEntryLock {
 		if ($this->acquired) {
 			$sql = "delete from $wpdb->options where option_name = %s";
 			$wpdb->query($wpdb->prepare($sql, $this->lock_id));
+			$this->acquired = false;
+		}
+	}
+
+}
+
+/**
+ * implementation for lock via persistent object cache
+ */
+class EntryLockObjectCache implements IEntryLock {
+
+	private string	$lock_id;
+	private bool	$acquired = false;
+
+	public function __construct(string $id) {
+		$this->lock_id = 'gfpxpay_elock_' . $id;
+	}
+
+	public function acquire() : bool {
+		if ($this->acquired) {
+			return true;
+		}
+
+		$this->acquired = wp_cache_add($this->lock_id, time(), OBJECT_CACHE_LOCK_GROUP, MINUTE_IN_SECONDS * 30);
+
+		return $this->acquired;
+	}
+
+	public function is_acquired() : bool {
+		return $this->acquired;
+	}
+
+	public function release() {
+		if ($this->acquired) {
+			wp_cache_delete($this->lock_id, OBJECT_CACHE_LOCK_GROUP);
 			$this->acquired = false;
 		}
 	}
