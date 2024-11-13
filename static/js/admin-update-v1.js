efd867df1f5c8ae03202ca8c23a705ce0619ce21
Ross McKay <rmckay@webaware.com.au>
2024-11-13 13:10:59 +1100

    compile js

--- static/js/admin-update-v1.js
+++ static/js/admin-update-v1.js
@@ -1,17 +1,15 @@
 (function ($) {
-  var clicked = false;
-  var ids = [];
-  var step_next = 0;
+  let clicked = false;
+  let ids = [];
+  let step_next = 0;
   $("#gfdpspxpay-upgrade").on("click", function () {
     if (!clicked) {
       clicked = true;
       processStep();
     }
   });
-
   function processStep() {
-    var step = gfdpspxpay_updatev1[step_next];
-
+    const step = gfdpspxpay_updatev1[step_next];
     if (step === "end") {
       $("#gfdpspxpay-upgrade").replaceWith("<strong>Done.</strong>");
       $("#gfdpspxpay-updatev1-progress").hide();
@@ -24,14 +22,12 @@
       }, processList);
     }
   }
-
   function processList(response) {
     if (response && response.success) {
       ids = response.data.ids;
       processNext(0, response.data.step);
     }
   }
-
   function processNext(next, step) {
     if (next < ids.length) {
       $.getJSON(ajaxurl, {
@@ -42,12 +38,10 @@
       }, processItem);
     }
   }
-
   function processItem(response) {
     if (response && response.success) {
-      var next = response.data.next;
-      var pcnt;
-
+      const next = response.data.next;
+      let pcnt;
       if (next < ids.length) {
         pcnt = Math.floor(next / ids.length * 100) + "%";
         processNext(next, response.data.step);
@@ -56,12 +50,11 @@
         step_next++;
         processStep();
       }
-
       $("#gfdpspxpay-updatev1-progress > div").css({
         width: pcnt
       }).text(pcnt);
     } else {
-      var error = response.data.error || "ERROR";
+      const error = response.data.error || "ERROR";
       $("#gfdpspxpay-updatev1-progress > div").css({
         width: "100%",
         backgroundColor: "transparent"
